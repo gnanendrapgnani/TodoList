b213edce06849a10a5d164eec0b804d138fc144d
gnanendrapgnani <gnanendrapnani@gmail.com>
2024-08-01 23:53:59 +0530

    used useReduce and deleted useState and increase performance

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -3,30 +3,18 @@ import TodoHeading from "./TodoHeading";
 import TodoAdd from "./TodoAdd";
 import TodoList from "./TodoList";
 import WelcomeMsg from "./WelcomeMsg";
-import { useState } from "react";
-import { TodoWorks } from "../stores/todo-stores";
+import TodoItemsContextProvider from "../stores/todo-stores";
 
 function TodoContainer() {
-  const [todo, setTodo] = useState([]);
-
-  const addTodo = (task, date) => {
-    setTodo((currValue) => [...currValue, { name: task, date: date }]);
-  };
-
-  const deleteTodo = (todoTask) => {
-    const newTask = todo.filter((task) => task.name !== todoTask);
-    setTodo(newTask);
-  };
-
   return (
-    <TodoWorks.Provider value={{ todo, addTodo, deleteTodo }}>
+    <TodoItemsContextProvider>
       <div className={style.container}>
         <TodoHeading />
         <TodoAdd />
         <WelcomeMsg />
         <TodoList />
       </div>
-    </TodoWorks.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
--- src/stores/todo-stores.jsx
+++ src/stores/todo-stores.jsx
@@ -1,7 +1,54 @@
 import { createContext } from "react";
+import { useReducer } from "react";
 
 export const TodoWorks = createContext({
   todo: [],
   addTodo: () => {},
   deleteTodo: () => {},
 });
+
+const todoReducer = (currTodo, action) => {
+  let newTodo = currTodo;
+
+  if (action.type === "NEW_TODO") {
+    newTodo = [
+      ...currTodo,
+      { name: action.payload.task, date: action.payload.date },
+    ];
+  } else if (action.type === "DELETE_TODO") {
+    newTodo = currTodo.filter((task) => task.name !== action.payload.task);
+  }
+  return newTodo;
+};
+
+const TodoItemsContextProvider = ({ children }) => {
+  const [todo, dispatchTodo] = useReducer(todoReducer, []);
+
+  const addTodo = (task, date) => {
+    const newTodoAction = {
+      type: "NEW_TODO",
+      payload: {
+        task,
+        date,
+      },
+    };
+    dispatchTodo(newTodoAction);
+  };
+
+  const deleteTodo = (task) => {
+    const deleteTodoAction = {
+      type: "DELETE_TODO",
+      payload: {
+        task: task,
+      },
+    };
+    dispatchTodo(deleteTodoAction);
+  };
+  return (
+    <TodoWorks.Provider value={{ todo, addTodo, deleteTodo }}>
+      {children}
+    </TodoWorks.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
